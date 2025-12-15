aeee36bd7651bfe297817ffdf49ffc1ddb6c1b3a
Ahmad Muzzammil Bin Mohd Sopi <abin-moh@u90z04s03.42kl.edu.my>
2025-12-15 15:21:51 +0800

    1

--- TestServer.cpp
+++ TestServer.cpp
@@ -11,11 +11,11 @@ void TestServer::accepter()
 	struct sockaddr_in address = get_socket()->get_address();
 	int addrlen = sizeof(address);
 	new_socket = accept(get_socket()->get_sock(), (struct sockaddr *)&address, (socklen_t *)&addrlen);
-	read(new_socket, buffer, sizeof(buffer));
 }
 
 void TestServer::handler()
 {
+	read(new_socket, buffer, sizeof(buffer));
 	std::cout << buffer << std::endl;
 }
 
